6b9d9fa34a17ff9380e04e87f118627c2596f063
ramtin bariran <bariran.ramtin@gmail.com>
2026-08-26 12:58:23 +0330

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -27,10 +27,7 @@
     @vite([
         'resources/css/app.css',
         'resources/css/brand.css',
-        'resources/css/theme-overrides.css',
-        'resources/css/menu-redesign.css',
         'resources/js/app.js',
-        'resources/js/menu-redesign.js',
     ])
 </head>
 <body class="min-h-dvh antialiased">
